384e024e93d4ac68d0724d5cedab856d431a1fae
Zolfikaar <fukar.work@gmail.com>
2024-01-28 07:29:13 +0300

    Complete checkout modal main style

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,8 +1,11 @@
 <script setup>
 import {ref,onMounted} from 'vue'
 import iconCashOn from '@/components/icons/iconCashOn.vue'
+import iconCheck from '@/components/icons/iconCheck.vue'
 
 let cart = ref([]);
+let cartLength = ref()
+let shortView = ref(true)
 let total = ref(0);
 let shipping = ref(50);
 let vat = ref(0);
@@ -12,6 +15,7 @@ onMounted(async () => {
   const storedCart = localStorage.getItem('cart');
   if (storedCart) {
     cart.value = JSON.parse(storedCart)
+    cartLength.value = cart.value !== null ? cart.value.length : 0
     total.value = cart.value.reduce((acc, item) => acc + item.price * item.quantity, 0)
     // Calculate VAT based on 20% of the product total (excluding shipping)
     vat.value = Math.floor((total.value - shipping.value) * 0.2)
@@ -57,31 +61,72 @@ let emptyEmoneyNum = ref(false)
 let invalidEmoneyNum = ref(false)
 let emptyEmoneyPin = ref(false)
 let invalidEmoneyPin = ref(false)
+
+let showModal = ref(false)
 const handleCheckoutInfo = () => {
 
-  if(name.value === ''){
+  let data = {
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    address: address.value,
+    zip: zip.value,
+    city: city.value,
+    country: country.value,
+    emoney_num: emoney_num.value,
+    emoney_pin: emoney_pin.value,
+  }
+
+  let validInfo = checkValidation(data)
+
+
+  if(!validInfo) {
+
+    showModal.value = true
+
+  }
+
+}
+
+function isValidEmail(email) {
+  // Regular expression for a basic email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const checkValidation = (data) => {
+  let checkoutInfoValid = false
+
+  let nameLength = data.name.toString().length
+  let validName = false
+  if(data.name === ''){
     emptyName.value = true
-  } else if (name.value.length < 6) {
+    shortName.value = false
+  } else if (nameLength < 6) {
     shortName.value = true
     emptyName.value = false
   } else {
     emptyName.value = false
     shortName.value = false
+    validName = true
   }
 
-  if(email.value === ''){
+  let validEmail = false
+  if(data.email === ''){
     emptyEmail.value = true
     notValidEmail.value = false
-  } else if (!isValidEmail(email.value)) {
+  } else if (!isValidEmail(data.email)) {
     notValidEmail.value = true
     emptyEmail.value = false
   } else {
     emptyName.value = false
     notValidEmail.value = false
+    validEmail = true
   }
   
-  let phoneNumberLength = phone.value.toString().length
-  if(phone.value === ''){
+  let phoneNumberLength = data.phone.toString().length
+  let vaildPhone = false
+  if(data.phone === ''){
     emptyPhoneNum.value = true
     shortPhoneNum.value = false
   } else if(phoneNumberLength < 8) {
@@ -90,16 +135,20 @@ const handleCheckoutInfo = () => {
   } else {
     emptyPhoneNum.value = false
     shortPhoneNum.value = false
+    vaildPhone = true
   }
 
-  if(address.value === ''){
+  let vaildAddress = false
+  if(data.address === ''){
     emptyAddress.value = true
   } else {
     emptyAddress.value = false
+    vaildAddress = true
   }
 
-  let zipCodeLength = zip.value.toString().length
-  if(zip.value === ''){
+  let zipCodeLength = data.zip.toString().length
+  let vaildZip = false
+  if(data.zip === ''){
     emptyZipCode.value = true
     invalidZipCode.value = false
   } else if(zipCodeLength !== 5){
@@ -108,23 +157,28 @@ const handleCheckoutInfo = () => {
   } else {
     emptyZipCode.value = false
     invalidZipCode.value = false
-
+    vaildZip = true
   }
 
-  if(city.value === '') {
+  let validCity = false
+  if(data.city === '') {
     emptyCity.value = true
   }else{
     emptyCity.value = false
+    validCity = true
   }
 
-  if(country.value === '') {
+  let validCountry = false
+  if(data.country === '') {
     emptyCountry.value = true
   }else{
     emptyCountry.value = false
+    validCountry = true
   }
   
-  let EmoneyNumLength = emoney_num.value.toString().length
-  if(emoney_num.value === ''){
+  let EmoneyNumLength = data.emoney_num.toString().length
+  let validEmNum = false
+  if(data.emoney_num === ''){
     emptyEmoneyNum.value = true
     invalidEmoneyNum.value = false
   } else if(EmoneyNumLength !== 9){
@@ -133,11 +187,12 @@ const handleCheckoutInfo = () => {
   } else {
     emptyEmoneyNum.value = false
     invalidEmoneyNum.value = false
-
+    validEmNum = true
   }
 
-  let EmoneyPinLength = emoney_pin.value.toString().length
-  if(emoney_pin.value === ''){
+  let EmoneyPinLength = data.emoney_pin.toString().length
+  let validEmpin = false
+  if(data.emoney_pin === ''){
     emptyEmoneyPin.value = true
     invalidEmoneyPin.value = false
   } else if(EmoneyPinLength !== 4){
@@ -146,19 +201,30 @@ const handleCheckoutInfo = () => {
   } else {
     emptyEmoneyPin.value = false
     invalidEmoneyPin.value = false
-
+    validEmpin = true
   }
-}
 
-function isValidEmail(email) {
-  // Regular expression for a basic email format
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+  if (paymentSelected == 'emoney') {
+    if(validName && validEmail && vaildPhone && vaildAddress && vaildZip && validCity && validCountry && validEmNum && validEmpin) {
+      checkoutInfoValid = true
+    } else {
+      checkoutInfoValid = false
+    }
+  } else {
+    if(validName && validEmail && vaildPhone && vaildAddress && vaildZip && validCity && validCountry) {
+      checkoutInfoValid = true
+    } else {
+      checkoutInfoValid = false
+    }
+  }
+
+  return checkoutInfoValid
 }
 </script>
 
 <template>
   <div class="wrapper">
+
     <div class="checkout-page">
       <div class="checkout-header">
         <a class="backBtn-anchor" @click="$router.go(-1)">
@@ -325,6 +391,61 @@ function isValidEmail(email) {
     </div>
 
   </div>
+
+  <div class="modal-wrapper" v-if="showModal">
+    <div class="checkout-modal">
+      <iconCheck />
+      <h3>THANK YOU <br> FOR YOUR ORDER</h3>
+      <p>You will receive an email confirmation shortly.</p>
+      <div class="cart-box">
+        <div class="left-side">
+
+          <div class="cart-box-items" v-if="shortView">
+            <div class="cart-box-item">
+              <div class="item-image">
+                <img :src="'/src/' + cart[0].image" alt="">
+              </div>
+              <div class="item-name-and-price">
+                <div class="item-name">{{ cart[0].name }}</div>
+                <div class="item-price">${{ cart[0].price.toLocaleString() }}</div>
+              </div>
+              <div class="item-quantity">x{{ cart[0].quantity }}</div>
+            </div>
+          </div>
+
+          <div class="cart-box-items" v-else>
+            <div class="cart-box-item" v-for="item in cart" :key="item">
+              <div class="item-image">
+                <img :src="'/src/' + item.image" alt="">
+              </div>
+              <div class="item-name-and-price">
+                <div class="item-name">{{ item.name }}</div>
+                <div class="item-price">${{ item.price.toLocaleString() }}</div>
+              </div>
+              <div class="item-quantity">x{{ item.quantity }}</div>
+            </div>
+          </div>
+
+          <hr>
+          <div 
+            class="cart-box-footer" 
+            @click="shortView = !shortView" 
+            >
+            and 2 other item(s)
+            <!-- @click="shortView = !shortView" 
+            v-if="shortView ? `and ${cartLength - 1 } other ` +  `${(cartLength - 1) > 1 }` ? `item(s)` : `item`
+            : 'Show less'" -->
+          </div>
+        </div>
+        <div class="right-side">
+          <div>GRAND TOTAL</div>
+          <div>$ {{ grandTotal.toLocaleString() }}</div>
+        </div>
+      </div>
+      <router-link :to="{name: 'home'}"><button class="btn1">BACK TO HOME</button></router-link>
+    </div>
+  </div>
+
 </template>
 
 <style >
@@ -451,13 +572,10 @@ function isValidEmail(email) {
 .checkout-page .checkout-content .checkout-form form .form-group.on-delivery-info,
 .checkout-page .checkout-content .checkout-form form .form-group.emoney-info{margin: 30px 0;}
 .checkout-page .checkout-content .checkout-form form .form-group.on-delivery-info{display: flex;align-items: center;}
-.checkout-page .checkout-content .checkout-form form .form-group.on-delivery-info p{
-  margin-left: 20px;
-}
+.checkout-page .checkout-content .checkout-form form .form-group.on-delivery-info p{margin-left: 20px;}
 /* ========================================================== */
 /* ==================== Cart Summary ======================== */
 /* ========================================================== */
-
 .cart-summary{height: fit-content;}
 .cart-summary .cart{
   width: 377px;
@@ -518,4 +636,100 @@ function isValidEmail(email) {
 .cart-summary .cart .cart-wrapper .totals .vat span:nth-child(2){font-weight: bold;font-size: 18px;}
 .cart-summary .cart .cart-wrapper .totals .grand-total span:nth-child(2){font-weight: bold;font-size: 18px;}
 .cart-summary .cart .cart-wrapper .pay-btn{width: 100%;}
+/* ========================================================== */
+/* ==================== Checkout Modal ======================== */
+/* ========================================================== */
+.modal-wrapper{
+  position: absolute;
+  top: 80px;
+  left: 0;
+  width: 100%;
+  height:calc(100% - 80px);
+  background-color: rgba(0, 0, 0,.5);
+  z-index: 3;
+}
+.checkout-modal{
+  position: relative;
+  top: 5%;
+  left: 50%;
+  transform: translate(-50%, -5%);
+  width: 540px;
+  background-color: white;
+  border-radius: 10px;
+  z-index: 4;
+  padding: 50px;
+}
+.checkout-modal h3{margin: 30px 0;}
+.checkout-modal p{margin-bottom: 40px;}
+.checkout-modal .cart-box{
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+  margin-bottom: 40px;
+}
+.checkout-modal .cart-box .left-side{
+  width: 55%;
+  background-color: var(--light-gray);
+  padding: 20px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+.checkout-modal .cart-box .left-side hr{border-color: rgba(0, 0, 0,.2);}
+.checkout-modal .cart-box .left-side .cart-box-items{}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item{
+  display: flex;
+  /* justify-content: space-between; */
+}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-image{margin-right: 25px;}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-image img{
+  width: 50px;
+  height: 50px;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-name-and-price{margin-right: 65px;}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-name-and-price .item-name,
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-name-and-price .item-price,
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-quantity{
+  font-weight: bold;
+  line-height: 25px;
+}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-name-and-price .item-name{font-size: 15px;}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-name-and-price .item-price{font-size: 14px;color: rgba(0, 0, 0,.5);}
+.checkout-modal .cart-box .left-side .cart-box-items .cart-box-item .item-quantity{text-align: right;color: rgba(0, 0, 0,.5);}
+
+.checkout-modal .cart-box .cart-box-footer{
+  margin-top: 10px ;
+  text-align: center;
+  color: rgba(0, 0, 0,.5);
+  font-size: 12px;
+  letter-spacing: -.21px;
+  font-weight: bold;
+}
+.checkout-modal .cart-box .cart-box-footer:hover{cursor: pointer;}
+.checkout-modal .cart-box .right-side{
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  padding: 20px;
+  width: 45%;
+  background-color: black;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+.checkout-modal .cart-box .right-side div:first-child{
+  font-size: 15px;
+  font-weight: normal;
+  color: rgba(255, 255, 255,.5);
+  line-height: 25px;
+  margin-bottom: 10px;
+}
+.checkout-modal .cart-box .right-side div:nth-child(2){
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  margin-bottom: 20px;
+}
+.checkout-modal a button{width: 100%;}
 </style>
